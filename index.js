683fb4ca1a969a6d08f0ab5b35e87187f5810146
Robert Bubnik <rbubnik@gmail.com>
2023-08-09 18:42:51 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData (userName, userEmail){
+return fetch('http://localhost:3000/users', {
+    method:'POST',
+    headers: {
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+    },
+    body: JSON.stringify({
+        name:userName,
+        email:userEmail
+    })
+})
+.then(response => response.json())
+.then( function ( object ) {
+    document.body.innerHTML = object[ "id" ]
+ 
+
+})
+.catch( function ( error ) {
+    document.body.innerHTML = error.message
+  } )
+
+}
+
+
+
+  
+ 
+
+//   const newP = document.createElement("p");
+//  const body = document.querySelector('body');
+//  body.appendChild(newP);
+// const  pTag = document.getElementsByTagName('p')[0]
+// pTag.innerHTML = `${data.name}  ${data.email}`
